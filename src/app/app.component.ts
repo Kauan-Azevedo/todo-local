e9f2f9e647146681f26e53e940bbb16ac5a4935f
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2023-01-02 15:22:09 -0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'sidebar-todo';
+  isOpen = false;
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['isOpen']) {
+      this.openNav();
+    } else {
+      this.closeNav();
+    }
+  }
+
+  openNav() {
+    this.isOpen = true;
+  }
+
+  closeNav() {
+    this.isOpen = false;
+  }
 }
